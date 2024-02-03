2c5784c73d7d672fcf00e3ee372e24d54933fadd
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-02-03 15:58:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import router from './src/mvc/route/user.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const port =process.env.PORT || 3001;
-const corsOptions={
+let corsOptions={
     origin : '*',
     OptionsSuccessStatus:200
 }
